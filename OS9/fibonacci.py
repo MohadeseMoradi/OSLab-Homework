d4c4dfc1c5348c5f893674c1518025ac86fe2505
MohadeseMoradi <76163394+MohadeseMoradi@users.noreply.github.com>
2020-12-31 09:38:01 +0330

    fibonacci.py

---
+++ OS9/fibonacci.py
@@ -0,0 +1,14 @@
+#fibo
+
+def fibo(n):
+    if n==1:
+        return 0;
+    if n==2:
+        return 1;
+    else:
+        return(fibo(n-1)+fibo(n-2))
+
+
+num=int(input())
+for i in range(1,num):
+    print(fibo(i))
