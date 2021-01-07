f19c99c9a6fe0ebf0f967585c6cafab8fc0c8843
MohadeseMoradi <76163394+MohadeseMoradi@users.noreply.github.com>
2021-01-07 10:16:55 +0330

    OS10

--- OS10/1.sh
+++ OS10/1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Enter your age please"
 read A
-if [$A -gt 18] || [$A -eq 18]
+if [ $A -gt 18 ] || [ $A -eq 18 ]
 then
 	echo "You may go to the party"
-elif [$A -lt 18]
+elif [ $A -lt 18 ]
 then
 	echo "Do you have letter?"
 	read L
-	if [$L = "yes"]
+	if [ $L = "yes" ]
 	then 
 		echo "You may go to the party but back befor midnight"
-	elif [ $L="no" ]
+	elif [ $L = "no" ]
 	then
 		echo "You may not go to the party." 
 	fi
